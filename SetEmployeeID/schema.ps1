66a8a1ed598c85a835e43d7e63921b92bd744559
Alcodowi <74603532+Alcodowi@users.noreply.github.com>
2021-09-10 15:48:37 +0200

    qaze

--- SetEmployeeID/schema.ps1
+++ SetEmployeeID/schema.ps1
@@ -2,7 +2,6 @@ $obj = New-Object -Type PSCustomObject
 $obj | Add-Member -Type NoteProperty -Name "Anchor-SamAccountName|String" -Value "00000000"
 $obj | Add-Member -Type NoteProperty -Name "objectClass|String" -Value "Externaluser"
 $obj | Add-Member -Type NoteProperty -Name "DistinguishedName|String" -Value ""
-$obj | Add-Member -Type NoteProperty -Name "SamAccountName|String" -Value ""
 $obj | Add-Member -Type NoteProperty -Name "employeeType|String" -Value ""
 $obj | Add-Member -Type NoteProperty -Name "msExchExtensionCustomAttribute1|String" -Value ""
 $obj | Add-Member -Type NoteProperty -Name "mail|String" -Value ""
